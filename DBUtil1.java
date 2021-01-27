c0aeb7b36f15b7ec1f09d999582277c14ae96a47
SindhuPoka <69710205+SindhuPoka@users.noreply.github.com>
2021-01-27 20:12:06 +0530

    Create DBUtil1.java

---
+++ DBUtil1.java
@@ -0,0 +1,19 @@
+package klu.connections;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class DBUtil {
+  final static String forNameURL="com.mysql.cj.jdbc.Driver";
+  final static String dBURL="jdbc:mysql://localhost:3306/epskill";
+  final static String username="root";
+  final static String password="root";
+  public static Connection DBConnection() throws SQLException,ClassNotFoundException{
+	  Class.forName("com.mysql.cj.jdbc.Driver");
+	  Connection con=DriverManager.getConnection(dBURL,username,password);
+	  return con;
+  }
+
+}
+
