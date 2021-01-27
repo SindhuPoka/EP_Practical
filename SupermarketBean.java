ae4f73765a74aec43e95010f13e688af811f940f
SindhuPoka <69710205+SindhuPoka@users.noreply.github.com>
2021-01-27 20:11:20 +0530

    Create SupermarketBean.java

---
+++ SupermarketBean.java
@@ -0,0 +1,27 @@
+package klu.bean;
+
+public class SupermarketBean {
+	private int itemid;
+	private String itemname;
+	private double cost;
+	public int getItemid() {
+		return itemid;
+	}
+	public void setItemid(int itemid) {
+		this.itemid = itemid;
+	}
+	public String getItemname() {
+		return itemname;
+	}
+	public void setItemname(String itemname) {
+		this.itemname = itemname;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
+
+}
