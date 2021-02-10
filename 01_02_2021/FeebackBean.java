c238cf6af56460d21011a8c224213f49020225a9
SindhuPoka <69710205+SindhuPoka@users.noreply.github.com>
2021-02-10 21:14:20 +0530

    Create FeebackBean.java

---
+++ 01_02_2021/FeebackBean.java
@@ -0,0 +1,55 @@
+
+public class FeedbackBean {
+  private String Q1,Q2,Q3,Q4,Q5;
+
+public String getQ1() {
+	// TODO Auto-generated method stub
+	return Q1;
+}
+
+public String getQ2() {
+	// TODO Auto-generated method stub
+	return Q2;
+}
+
+public String getQ3() {
+	// TODO Auto-generated method stub
+	return Q3;
+}
+
+public String getQ4() {
+	// TODO Auto-generated method stub
+	return Q4;
+}
+
+public String getQ5() {
+	// TODO Auto-generated method stub
+	return Q5;
+}
+
+public void setQ1(String q12) {
+	// TODO Auto-generated method stub
+	Q1=q12;
+}
+
+public void setQ3(String q32) {
+	// TODO Auto-generated method stub
+	Q3=q32;
+}
+
+public void setQ2(String q22) {
+	// TODO Auto-generated method stub
+	Q2=q22;
+}
+
+public void setQ4(String q42) {
+	Q4=q42;
+	
+}
+
+public void setQ5(String q52) {
+	Q5=q52;
+	
+}
+
+}
